c8158228790589952e2f97c231e9d92944b523e6
samibinol <119807647+samibinol@users.noreply.github.com>
2022-12-06 11:02:09 +0100

    Adding comments to main.py

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ from dotenv import load_dotenv
 import logging
 import os
 
+# Activating logging
+# TODO: changing logging status with -d argument 
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
 )
 logger = logging.getLogger(__name__)
 
+# Loading the API Key from the ENV file
+
 load_dotenv()
 token = os.getenv('TELEGRAM_API_KEY')
 
+
+# Commands 
 
 def start_command(update, context):
     update.message.reply_text('Hello!')
@@ -22,15 +28,21 @@ def add_sticker(update, context):
     pass
 
 
+# TODO: adding the inline function
+
 """
 def inline_query(update, context):
     query = update.inline_query.query
 """
 
 
+# Catch errors from python-telegram-bot
+
 def error(update, context):
     print(f"Update {update} caused error {context.error}")
 
+
+# main() function of python-telegram-bot
 
 def main():
     updater = Updater(token, use_context=True)
